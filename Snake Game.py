f5994d0b759b38e6547d5a6a0b19c21e422efb0d
hashAMAN <jaiswal.amankumar9898@gmail.com>
2022-04-06 13:29:27 +0530

    First Commit

---
+++ Snake Game.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+import random
+delay=0.1
+score=0
+high_score=0
+wn=turtle.screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
